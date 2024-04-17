9ce8e71c0db1a45cd189c8527021be9a2b88de75
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-04-17 14:20:26 +0200

    Dodavanje nove stavke za kupovinu

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ let editView = document.querySelector('#edit-wishes-view')
 
 // buttons
 let allNavBtns = document.querySelectorAll('nav a');
+let saveBtn = document.querySelector('#save-btn');
+
+// inputs
+let itemInput = document.querySelector('input[name="item"]');
+let priceInput = document.querySelector('input[name="price"]');
+let linkInput = document.querySelector('input[name="link"]');
+let shopInput = document.querySelector('input[name="shop"]');
+let currencyInput = document.querySelector('select[name="currency"]');
+let importantInput = document.querySelector('input[name="important"]');
 
 crateWishTable();
 
@@ -12,6 +21,7 @@ let allEditBtns = document.querySelectorAll('td button');
 
 
 // events
+saveBtn.addEventListener('click', saveNewWish);
 allNavBtns.forEach(btn => btn.addEventListener('click', function (e){
     if(e.preventDefault()){
         e.preventDefault();
@@ -21,6 +31,33 @@ allNavBtns.forEach(btn => btn.addEventListener('click', function (e){
 }))
 
 allEditBtns.forEach(btn => btn.addEventListener('click', showEditView))
+
+// functions
+
+function saveNewWish(e){
+    e.preventDefault()
+    let newWish ={
+        item : itemInput.value,
+        price : priceInput.value,
+        link : linkInput.value,
+        shop : shopInput.value,
+        currency : currencyInput.value,
+        important : importantInput.checked
+    }
+    console.log(newWish);
+    wishes.push(newWish);
+    crateWishTable();
+    console.log(wishes);
+    showView(`all-wishes-view`)
+
+    itemInput.value ='';
+    priceInput.value ='';
+    shopInput.value ='';
+    linkInput.value ='';
+    currencyInput.value ='';
+    importantInput.value ='';
+
+}
 
 function toggleBtn(btn){
     allNavBtns.forEach(btn =>{
@@ -39,7 +76,7 @@ function showView(viewId) {
     }
 }
 
-function showEditView(e){
+function showEditView(){
     allWishesViews.forEach(view => {
         view.style.display ='none'
     })
@@ -68,5 +105,4 @@ function crateWishTable(){
         `.trim();
     })
     allWishesTbody.innerHTML = text;
-    console.log(text);
 }
